1a4152f907cc8d4929dc1eb390779b8e42dd72b8
未来_加油 <741804631@qq.com>
2019-04-02 20:03:45 +0800

    打包测试代码展示

---
+++ precache-manifest.93342817f3a0734544ef53095da77891.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca2b1b2a669a67afb3df",
+    "url": "/reactjob/static/js/runtime~main.ca2b1b2a.js"
+  },
+  {
+    "revision": "711db89461de00f4a83d",
+    "url": "/reactjob/static/js/main.711db894.chunk.js"
+  },
+  {
+    "revision": "e844b03c91ae53f6700d",
+    "url": "/reactjob/static/js/1.e844b03c.chunk.js"
+  },
+  {
+    "revision": "711db89461de00f4a83d",
+    "url": "/reactjob/static/css/main.879fb0d3.chunk.css"
+  },
+  {
+    "revision": "e844b03c91ae53f6700d",
+    "url": "/reactjob/static/css/1.650962bf.chunk.css"
+  },
+  {
+    "revision": "de3d63e17cca0ed156e7de93c6e34bd2",
+    "url": "/reactjob/index.html"
+  }
+];
